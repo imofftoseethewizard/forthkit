a863aa75e90896c3d386cca301448b6e29974c1b
Pat Lasswell <imofftoseethewizard@gmail.com>
2022-05-26 07:08:53 -0700

    test monitor refinements

--- test/monitor.sh
+++ test/monitor.sh
@@ -8,6 +8,6 @@ inotifywait -mr --exclude '.*\.[cho]$' -e CLOSE_WRITE $FORTHKIT/build | \
         version_tag=$(echo $dir | sed -E 's|.*/([^/]*)/bin/|\1|')
         log_file=$FORTHKIT/test/log/$version_tag
 
-        date -Iseconds -u >>$log_file
-        VERSION_TAG=$version_tag ./runner.sh f79/core.rc >>$log_file
+        echo -n `date "+%Y-%m-%dT%H:%M:%SZ" -u` "" >>$log_file
+        VERSION_TAG=$version_tag ./runner.sh $FORTHKIT/test/f79/core.rc >>$log_file
     done
