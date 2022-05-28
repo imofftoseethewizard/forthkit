b969981d4733ec661e241b9c82eb53c56e1be93a
Pat Lasswell <imofftoseethewizard@gmail.com>
2022-05-28 08:41:14 -0700

    fix type size mismatch for c8/rel builds

--- address/relocatable.c
+++ address/relocatable.c
@@ -1,7 +1,7 @@
 #define _to_ptr(x)   (cell *)((char *)e + (x))
 #define _from_ptr(x) (cell)((char *)(x) - (char *)e)
 
-#define c_primitive_flag (1L << (sizeof(cell) * 8 - 1))
+#define c_primitive_flag ((cell)1 << (sizeof(cell) * 8 - 1))
 
 #define _is_primitive(x)  (((long)(x)) & c_primitive_flag)
 #define _from_pv(x) (void *)((x) - _pr_value_base | c_primitive_flag)
