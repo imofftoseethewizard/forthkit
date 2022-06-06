c85fe162a517fcfb145d12b9480b84a70944aec7
Pat Lasswell <imofftoseethewizard@gmail.com>
2022-06-06 07:01:22 -0700

    reorg build w/ FAMILY, move most interpreters/f79/ files to interpreters/

--- interpreters/build-monitor.sh
+++ interpreters/build-monitor.sh
@@ -9,9 +9,9 @@ log_file=$FORTHKIT/log/build
 
 make deps >/dev/null
 
-inotifywait -mr --fromfile $FORTHKIT/build/$FAMILY/deps.txt -e CLOSE_WRITE $FORTHKIT/address $FORTHKIT/execution-models $FORTHKIT/interpreters/$FAMILY | \
+inotifywait -mr --fromfile $FORTHKIT/build/$FAMILY/deps.txt -e CLOSE_WRITE $FORTHKIT/address $FORTHKIT/execution-models $FORTHKIT/interpreters | \
     while read dir events file
     do
         echo $dir $events $file >>$log_file
-        $FORTHKIT/interpreters/$FAMILY/build-all-versions.sh >>$log_file
+        $FORTHKIT/interpreters/build-all-versions.sh >>$log_file
     done
