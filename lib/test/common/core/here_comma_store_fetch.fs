57f7bb921118c126e6d9ef8da6e5c9cbac2e3e9b
Pat Lasswell <imofftoseethewizard@gmail.com>
2022-06-09 05:37:58 -0700

    add doc to ! "store", revise tests

--- lib/test/common/core/here_comma_store_fetch.fs
+++ lib/test/common/core/here_comma_store_fetch.fs
@@ -4,12 +4,12 @@ here 2 ,
 constant 2nd
 constant 1st
 
-t{       1st 2nd <  -> 1 2 < }t  ( todo u<                    )
-t{       1st cell+  -> 2nd   }t
-t{   1st 1 cells +  -> 2nd   }t
-t{     1st @ 2nd @  -> 1 2   }t
-t{         5 1st !  ->       }t
-t{     1st @ 2nd @  -> 5 2   }t
-t{         6 2nd !  ->       }t
-t{     1st @ 2nd @  -> 5 6   }t
-t{ 1   1st ! 1st @  -> 1     }t  ( can store cell-wide value  )
+t{      1st 2nd u<  -> <true> }t
+t{       1st cell+  -> 2nd    }t
+t{   1st 1 cells +  -> 2nd    }t
+t{     1st @ 2nd @  -> 1 2    }t
+t{         5 1st !  ->        }t
+t{     1st @ 2nd @  -> 5 2    }t
+t{         6 2nd !  ->        }t
+t{     1st @ 2nd @  -> 5 6    }t
+t{ 1s  1st ! 1st @  -> 1s     }t  ( can store cell-wide value  )
--- primitive/core/store.m4
+++ primitive/core/store.m4
@@ -1,7 +1,15 @@
+/*
+    ! ( n addr -- ) "store"
+        Store n at addr.
+*/
 #define _store() *_to_ptr(*sp) = *(sp+1), sp += 2
 
-__primitive(pr_store)  
+__primitive(pr_store)
 {
+    /*
+        ! ( n addr -- ) "store"
+            Store n at addr.
+    */
     _store();
 }
 __end
