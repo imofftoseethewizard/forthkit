d9ae91a4da7e96f014c3353ee074db115f10dbcc
Pat Lasswell <imofftoseethewizard@gmail.com>
2022-06-17 07:47:12 -0700

    added monitor.sh, improved build-all-versions.sh with error output

--- scripts/build-all-versions.sh
+++ scripts/build-all-versions.sh
@@ -8,13 +8,13 @@ do
     do
         export VERSION_TAG=$(echo $f | sed -E 's|.*/([^.]*)\.rc$|\1|')
         . $f
-        FAMILY=$family make deps all >/dev/null 2>&1
+        FAMILY=$family make deps all 2>&1 | grep 'error: '
 
         if [ $? = '0' ]
         then
-            echo `date "+%Y-%m-%dT%H:%M:%SZ" -u` Built $family version $VERSION_TAG successfully.
-        else
             echo `date "+%Y-%m-%dT%H:%M:%SZ" -u` Build of $family  version $VERSION_TAG failed.
+        else
+            echo `date "+%Y-%m-%dT%H:%M:%SZ" -u` Built $family version $VERSION_TAG successfully.
         fi
     done
 done
