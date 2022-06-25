180b374746838b86b20f0d2947cfb7b037d57277
Pat Lasswell <imofftoseethewizard@gmail.com>
2022-06-25 05:09:43 -0700

    add debug configs

---
+++ interpreters/config/debug-c2-l1.h
@@ -0,0 +1,11 @@
+#define CELL_TYPE                short
+#define DOUBLE_TYPE              long
+#define LENGTH_TYPE              unsigned char
+
+/* Diagnostic output control */
+#define LOG                      1
+#define TRACE                    1
+#define VERBOSE                  1
+
+#define DEBUG                    1
+#define BOUNDS_CHECKING          1
---
+++ interpreters/config/debug-c8-l1.h
@@ -0,0 +1,11 @@
+#define CELL_TYPE                long long int
+#define DOUBLE_TYPE              __int128
+#define LENGTH_TYPE              unsigned char
+
+/* Diagnostic output control */
+#define LOG                      1
+#define TRACE                    1
+#define VERBOSE                  1
+
+#define DEBUG                    1
+#define BOUNDS_CHECKING          1
