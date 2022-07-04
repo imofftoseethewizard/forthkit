30e53186b8f8b07ed671871956c0e64d23dffcfd
Pat Lasswell <imofftoseethewizard@gmail.com>
2022-07-04 05:56:47 -0700

    different tests for bootstrap-interpreter vs bundled

--- scripts/test-monitor.sh
+++ scripts/test-monitor.sh
@@ -3,16 +3,23 @@
 inotifywait -mr --exclude 'log|.*\.[cho]$' -e CLOSE_WRITE $FORTHKIT/build | \
     while read dir events file
     do
-        if [ $file = 'bootstrap-interpreter' ]
+        if [ $file = 'interpreter' ] || [ $file = 'bootstrap-interpreter' ]
         then
             version_tag=$(echo $dir | sed -E 's|.*/([^/]+)/bin/|\1|')
 
             export FAMILY=$(echo $dir | sed -E "s|.*/([^/]*)/$version_tag/bin/|\1|")
+            export INTERPRETER=$file
+            export VERSION_TAG=$version_tag
 
             log_file=$FORTHKIT/log/test/$FAMILY/$version_tag
 
             mkdir -p $FORTHKIT/log/test/$FAMILY
 
-            VERSION_TAG=$version_tag $FORTHKIT/scripts/runner.sh $FORTHKIT/test/common/*.rc $FORTHKIT/test/$FAMILY/*.rc >>$log_file
+            $FORTHKIT/scripts/runner.sh >>$log_file  \
+                $FORTHKIT/test/common/*.rc           \
+                $FORTHKIT/test/$FAMILY/*.rc          \
+                $FORTHKIT/test/common/$file/*.rc     \
+                $FORTHKIT/test/$FAMILY/$file/*.rc
+
         fi
     done
