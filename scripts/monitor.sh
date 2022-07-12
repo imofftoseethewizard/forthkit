604d6591a29da5b996d61ec9dd7e4a87fa4aab60
Pat Lasswell <imofftoseethewizard@gmail.com>
2022-07-12 05:53:04 -0700

    better restart behavior for monitor.sh

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/env sh
 
+(ps -o pid,comm| egrep '(notifywait|monitor|sh|tail)$' | awk '{print $1}' | grep -v $$ | xargs kill)  >/dev/null 2>&1
+(find $FORTHKIT/build -name "deps.txt" | xargs rm) >/dev/null 2>&1
+($FORTHKIT/scripts/update-all-deps.sh) >/dev/null 2>&1
+
 $FORTHKIT/scripts/build-monitor.sh >/dev/null 2>&1 &
 $FORTHKIT/scripts/test-monitor.sh  >/dev/null 2>&1 &
 
