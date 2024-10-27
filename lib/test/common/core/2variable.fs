d264384a129b5e42efb1733f7fb64e4c254d0769
Pat Lasswell <imofftoseethewizard@gmail.com>
2024-10-27 09:09:32 -0700

    2variable plus tests

---
+++ lib/test/common/core/2variable.fs
@@ -0,0 +1,14 @@
+T{ 2VARIABLE 2v1 -> }T
+T{ 0 0 2v1 2! ->    }T
+T{    2v1 2@ -> 0 0 }T
+T{ -1 -2 2v1 2! ->       }T
+T{       2v1 2@ -> -1 -2 }T
+
+T{ : cd2 2VARIABLE ; -> }T
+T{ cd2 2v2 -> }T
+T{ : cd3 2v2 2! ; -> }T
+T{ -2 -1 cd3 -> }T
+T{ 2v2 2@ -> -2 -1 }T
+
+T{ 2VARIABLE 2v3 IMMEDIATE 5 6 2v3 2! -> }T
+T{ 2v3 2@ -> 5 6 }T
